571346be9c602945c9e997b642a42ff49acd6f1a
greym0uth <thejadenjack@gmail.com>
2022-08-09 00:04:20 -0700

    add highlighting

---
+++ queries/highlights.scm
@@ -0,0 +1,13 @@
+["module"] @keyword
+
+(comment) @comment
+(developer_comment) @comment
+
+(arguments) @property
+(attribute) @attribute
+(call_expression) @function
+(column_type) @type
+(enumeral) @constant
+(identifier) @variable
+(string) @string
+
